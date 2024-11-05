1c049ae214ee0c37fc0ca098abad7945dadcee76
Pedro-de-Souza <00001103917195sp@al.educacao.sp.gov.br>
2024-11-04 23:29:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,84 @@
+criaCartao(
+    'Filmes',
+    'Qual é o nome do vilão principal em "O Cavaleiro das Trevas" de 2008?',
+    'O vilão principal é o Coringa, interpretado por Heath Ledger'
+)
+
+criaCartao(
+    'Animes',
+    'Qual é o nome completo do protagonista de "Death Note"?',
+    'O nome completo do protagonista é Light Yagami'
+)
+
+criaCartao(
+    'TI',
+    'O que significa a sigla DNS?',
+    'DNS significa Domain Name System'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o conceito de recursividade em programação?',
+    'Recursividade é quando uma função chama a si mesma até atingir uma condição base'
+)
+
+criaCartao(
+    'Filmes',
+    'Em "Matrix", qual é a cor da pílula que Neo escolhe?',
+    'Neo escolhe a pílula vermelha'
+)
+
+criaCartao(
+    'Animes',
+    'Em "One Piece", qual é o objetivo de Luffy?',
+    'O objetivo de Luffy é encontrar o One Piece e se tornar o Rei dos Piratas'
+)
+
+criaCartao(
+    'TI',
+    'O que é a camada de transporte no modelo OSI?',
+    'A camada de transporte é responsável pela transferência de dados confiável entre sistemas'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a diferença entre uma classe e um objeto em programação orientada a objetos?',
+    'Uma classe é um modelo ou blueprint, enquanto um objeto é uma instância dessa classe'
+)
+
+criaCartao(
+    'Filmes',
+    'Quem foi o criador original da franquia "Star Wars"?',
+    'O criador original de Star Wars é George Lucas'
+)
+
+criaCartao(
+    'TI',
+    'O que é um algoritmo de hash criptográfico e cite um exemplo?',
+    'Um algoritmo de hash criptográfico converte dados em uma sequência fixa de caracteres. Um exemplo é o SHA-256'
+)
+
+
+criaCartao(
+    'TI',
+    'Qual é a diferença entre IPv4 e IPv6?',
+    'IPv4 usa endereços de 32 bits e suporta cerca de 4,3 bilhões de endereços, enquanto IPv6 usa endereços de 128 bits, permitindo um número muito maior de endereços'
+)
+
+criaCartao(
+    'Programação',
+    'O que são estruturas de dados imutáveis e por que são importantes em linguagens funcionais?',
+    'Estruturas de dados imutáveis são aquelas que, uma vez criadas, não podem ser alteradas. Elas são importantes em linguagens funcionais porque ajudam a evitar efeitos colaterais e facilitar a concorrência'
+)
+
+criaCartao(
+    'TI',
+    'O que é um firewall?',
+    'Um firewall é um sistema de segurança que monitora e controla o tráfego de rede'
+)
+
+criaCartao(
+    'Programação',
+    'O que é o Git?',
+    'Git é um sistema de controle de versão distribuído usado para rastrear mudanças no código-fonte'
+)
